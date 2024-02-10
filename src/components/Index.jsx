1f091e7ce070b10066061c3cbe9a213d533fbd41
REDUAN <reduan15-11413@diu.edu.bd>
2024-02-11 01:49:54 +0600

    update home page

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <div className="container">
             <div className="align-items-center justify-content-center row vh-100">
-                <div className="col-8">
+                <div className="">
                     <ul className="align-content-center flex-column gap-3 mb-3 nav nav-pills">
                         <li className="nav-item">
                             <NavLink to='/problem-1' className="nav-link active">Problem - 1</NavLink>
